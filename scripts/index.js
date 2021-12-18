b9fc385e141feb68bfe388971695ddba0e3aaf20
Anastasiya Danilova <danilovaness@yandex.ru>
2021-12-18 16:59:50 +0500

    feature: Добавить скрипты JS

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,49 @@ const popupOpenButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close-button');
 const popup = document.querySelector('.popup');
 
+const namePage = document.querySelector('.profile__name');
+const jobPage = document.querySelector('.profile__description');
 
-function openClose() {
-    popup.classList.toggle('popup_open');
+function popupOpen() {
+    popup.classList.add('popup_open');
+
+    nameInput.value = namePage.textContent;
+    jobInput.value = jobPage.textContent;
 }
 
-popupOpenButton.addEventListener('click',openClose);
-popupCloseButton.addEventListener('click',openClose);
+function popupClose() {
+    popup.classList.remove('popup_open')
+}
+
+popupOpenButton.addEventListener('click',popupOpen);
+popupCloseButton.addEventListener('click',popupClose);
+
+
+
+
+
+const popupForm = document.querySelector('.popup__form');
+
+const nameInput = popupForm.querySelector('.popup__popup-name');
+const jobInput = popupForm.querySelector('.popup__popup-disription');
+
+const popupButton = popupForm.querySelector('.submit-button');
+
+
+
+function saveNameJob (evt) {
+    
+    evt.preventDefault();
+
+    namePage.textContent = nameInput.value;
+    jobPage.textContent = jobInput.value;
+
+}
+
+popupButton.addEventListener('submit', saveNameJob); 
+popupButton.addEventListener('click', saveNameJob);
+popupButton.addEventListener('click', popupClose);
+
+
+
+
