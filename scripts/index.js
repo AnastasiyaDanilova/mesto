9fbc4f963421aa4ee826171a9dd4a79b924fb955
Anastasiya Danilova <danilovaness@yandex.ru>
2021-12-24 13:36:37 +0500

    BagFix: исправить ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,11 @@
 const popupOpenButton = document.querySelector('.button_type_edit-info');
 const popupCloseButton = document.querySelector('.button_type_close-popup');
 const popup = document.querySelector('.popup');
-
 const userName = document.querySelector('.profile__name');
 const userJob = document.querySelector('.profile__description');
-
 const popupForm = document.querySelector('.popup__form');
-
 const nameInput = popupForm.querySelector('.popup__input_type_name');
 const jobInput = popupForm.querySelector('.popup__input_type_job');
-
-const popupButton = popupForm.querySelector('.button_type_submit');
 
 function popupOpen() {
     nameInput.value = userName.textContent;
@@ -36,8 +31,3 @@ function formSubmitHandler (evt) {
 popupForm.addEventListener('submit', formSubmitHandler); 
 popupOpenButton.addEventListener('click',popupOpen);
 popupCloseButton.addEventListener('click',popupClose);
-
-
-
-
-
